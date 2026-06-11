b6621e9e7edb347c53965096817490c295e9b564
Bhavith k <bhavithkanaparthi@gmail.com>
2026-06-11 12:07:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,295 @@
+const dropdowns = document.querySelectorAll(".dropdown-container"),
+  inputLanguageDropdown = document.querySelector("#input-language"),
+  outputLanguageDropdown = document.querySelector("#output-language"),
+  recordBtn = document.querySelector(".record"),
+  fromLanguage = document.querySelector("#language");
+
+function populateDropdown(dropdown, options) {
+  dropdown.querySelector("ul").innerHTML = "";
+  options.forEach((option) => {
+    const li = document.createElement("li");
+    const title = option.name + " (" + option.native + ")";
+    li.innerHTML = title;
+    li.dataset.value = option.code;
+    li.classList.add("option");
+    dropdown.querySelector("ul").appendChild(li);
+  });
+}
+
+populateDropdown(inputLanguageDropdown, languages);
+populateDropdown(outputLanguageDropdown, languages);
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+    dropdown.classList.toggle("active");
+  });
+
+  dropdown.querySelectorAll(".option").forEach((item) => {
+    item.addEventListener("click", (e) => {
+      //remove active class from current dropdowns
+      dropdown.querySelectorAll(".option").forEach((item) => {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+      const selected = dropdown.querySelector(".selected");
+      selected.innerHTML = item.innerHTML;
+      selected.dataset.value = item.dataset.value;
+      translate();
+    });
+  });
+  const searchInput = dropdown.querySelector(".dropdown-search");
+searchInput.addEventListener("input", () => {
+  const searchValue = searchInput.value.toLowerCase();
+  dropdown.querySelectorAll(".option").forEach((option) => {
+    const text = option.innerText.toLowerCase();
+    if (text.includes(searchValue)) {
+      option.style.display = "block";
+    } else {
+      option.style.display = "none";
+    }
+  });
+});
+});
+
+document.addEventListener("click", (e) => {
+  dropdowns.forEach((dropdown) => {
+    if (!dropdown.contains(e.target)) {
+      dropdown.classList.remove("active");
+    }
+  });
+});
+
+const swapBtn = document.querySelector(".swap-position"),
+  inputLanguage = inputLanguageDropdown.querySelector(".selected"),
+  outputLanguage = outputLanguageDropdown.querySelector(".selected"),
+  inputTextElem = document.querySelector("#input-text"),
+  outputTextElem = document.querySelector("#output-text");
+
+swapBtn.addEventListener("click", (e) => {
+  const temp = inputLanguage.innerHTML;
+  inputLanguage.innerHTML = outputLanguage.innerHTML;
+  outputLanguage.innerHTML = temp;
+
+  const tempValue = inputLanguage.dataset.value;
+  inputLanguage.dataset.value = outputLanguage.dataset.value;
+  outputLanguage.dataset.value = tempValue;
+
+  //swap text
+  const tempInputText = inputTextElem.value;
+  inputTextElem.value = outputTextElem.value;
+  outputTextElem.value = tempInputText;
+
+  translate();
+});
+
+function translate() {
+  const inputText = inputTextElem.value;
+  const inputLanguage =
+    inputLanguageDropdown.querySelector(".selected").dataset.value;
+  const outputLanguage =
+    outputLanguageDropdown.querySelector(".selected").dataset.value;
+  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=${outputLanguage}&dt=t&q=${encodeURI(
+    inputText,
+  )}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      outputTextElem.value = json[0].map((item) => item[0]).join("");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+inputTextElem.addEventListener("input", (e) => {
+  //limit input to 5000 characters
+  if (inputTextElem.value.length > 5000) {
+    inputTextElem.value = inputTextElem.value.slice(0, 5000);
+  }
+  translate();
+});
+
+const uploadDocument = document.querySelector("#upload-document"),
+  uploadTitle = document.querySelector("#upload-title");
+
+uploadDocument.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  if (
+    file.type === "application/pdf" ||
+    file.type === "text/plain" ||
+    file.type === "application/msword" ||
+    file.type ===
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  ) {
+    uploadTitle.innerHTML = file.name;
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (e) => {
+      inputTextElem.value = e.target.result;
+      translate();
+    };
+  } else {
+    alert("Please upload a valid file");
+  }
+});
+
+const downloadBtn = document.querySelector("#download-btn");
+
+downloadBtn.addEventListener("click", (e) => {
+  const outputText = outputTextElem.value;
+  const outputLanguage =
+    outputLanguageDropdown.querySelector(".selected").dataset.value;
+  if (outputText) {
+    const blob = new Blob([outputText], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.download = `translated-to-${outputLanguage}.txt`;
+    a.href = url;
+    a.click();
+  }
+});
+
+const darkModeCheckbox = document.getElementById("dark-mode-btn");
+
+darkModeCheckbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
+
+const inputChars = document.querySelector("#input-chars");
+
+inputTextElem.addEventListener("input", (e) => {
+  inputChars.innerHTML = inputTextElem.value.length;
+});
+function speakText1() {
+  const inputLanguage = inputLanguageDropdown.querySelector(".selected").dataset.value;
+  const inputText = inputTextElem.value;
+  // Use SpeechSynthesis API to speak the translated text
+  const utterance = new SpeechSynthesisUtterance(inputText);
+  utterance.lang = inputLanguage;
+  speechSynthesis.speak(utterance);
+}
+document.getElementById('speak-icon1').addEventListener('click', speakText1);
+
+function copyText1() {
+  const inputText = inputTextElem.value;
+  navigator.clipboard.writeText(inputText)
+    .then(() => {
+      alert('Text copied to clipboard');
+    })
+    .catch((error) => {
+      console.error('Error copying text: ', error);
+    });
+}
+
+// Add event listener for the copy icon
+document.getElementById('copy-icon1').addEventListener('click', copyText1);
+
+function speakText() {
+  const outputLanguage = outputLanguageDropdown.querySelector(".selected").dataset.value;
+  const outputText = outputTextElem.value;
+  // Use SpeechSynthesis API to speak the translated text
+  const utterance = new SpeechSynthesisUtterance(outputText);
+  utterance.lang = outputLanguage;
+  speechSynthesis.speak(utterance);
+}
+
+// Add event listener for the speak icon
+document.getElementById('speak-icon').addEventListener('click', speakText);
+function copyText() {
+  const outputText = outputTextElem.value;
+  navigator.clipboard.writeText(outputText)
+    .then(() => {
+      alert('Text copied to clipboard');
+    })
+    .catch((error) => {
+      console.error('Error copying text: ', error);
+    });
+}
+
+// Add event listener for the copy icon
+document.getElementById('copy-icon').addEventListener('click', copyText);
+
+let SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition,
+  recognition,
+  recording = false;
+
+function populateLanguages() {
+  languages.forEach((lang) => {
+    const option = document.createElement("option");
+    option.value = lang.code;
+    option.innerHTML = lang.name;
+    fromLanguage.appendChild(option);
+  });
+}
+
+populateLanguages();
+
+function speechToText() {
+  try {
+    recognition = new SpeechRecognition();
+    recognition.lang = fromLanguage.value;
+    recognition.interimResults = true;
+    recordBtn.classList.add("recording");
+    recordBtn.querySelector("p").innerHTML = "Listening...";
+    recognition.start();
+    recognition.onresult = (event) => {
+      const speechResult = event.results[0][0].transcript;
+      //detect when intrim results
+      if (event.results[0].isFinal) {
+        inputTextElem.innerHTML += " " + speechResult;
+
+      } else {
+        //creative p with class interim if not already there
+        if (!document.querySelector(".interim")) {
+          const interim = document.createElement("p");
+          interim.classList.add("interim");
+          inputTextElem.appendChild(interim);
+        }
+        //update the interim p with the speech result
+        
+      }
+    
+    };
+    recognition.onspeechend = () => {
+      speechToText();
+    };
+    recognition.onerror = (event) => {
+      stopRecording();
+      if (event.error === "no-speech") {
+        alert("No speech was detected. Stopping...");
+      } else if (event.error === "audio-capture") {
+        alert(
+          "No microphone was found. Ensure that a microphone is installed."
+        );
+      } else if (event.error === "not-allowed") {
+        alert("Permission to use microphone is blocked.");
+      } else if (event.error === "aborted") {
+        alert("Listening Stopped.");
+      } else {
+        alert("Error occurred in recognition: " + event.error);
+      }
+    };
+  } catch (error) {
+    recording = false;
+
+    console.log(error);
+  }
+}
+
+recordBtn.addEventListener("click", () => {
+  if (!recording) {
+    speechToText();
+    recording = true;
+  } else {
+    stopRecording();
+  }
+});
+
+function stopRecording() {
+  recognition.stop();
+  recordBtn.querySelector("p").innerHTML = "Start Listening";
+  recordBtn.classList.remove("recording");
+  recording = false;
+}
+
